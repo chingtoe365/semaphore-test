6e49a9a2d9f85a62aa56859a9f8b29d5e37e08e8
vagrant <vagrant@precise64.(none)>
2017-06-29 11:27:39 +0000

    force yes bash

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
 #./deploy.sh
 
 # Install git/docker/docker-compose on AWS server (notice there might be prompts)
-sudo apt-get –-assume-yes install git
-sudo apt-get –-assume-yes remove docker docker-engine
-sudo apt-get –-assume-yes update
-sudo apt-get –-assume-yes install linux-image-extra-$(uname -r) linux-image-extra-virtual
-sudo apt-get –-assume-yes install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get --yes install git
+sudo apt-get --yes remove docker docker-engine
+sudo apt-get --yes update
+sudo apt-get --yes install linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get --yes install apt-transport-https ca-certificates curl software-properties-common
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add –
 sudo apt-key fingerprint OEBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) \
 stable"
 sudo apt-get update
-sudo apt-get –-assume-yes install docker-ce
+sudo apt-get --yes install docker-ce
 sudo -i
 curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 exit
